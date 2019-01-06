e5870efc7de062f2493f2e9c35e0e9bd30311435
noiach <gh@noiach.com>
2019-01-06 23:51:56 +1100

    Improved network block

--- src/components/statuspage/block/NetworkBlock.tsx
+++ src/components/statuspage/block/NetworkBlock.tsx
@@ -5,6 +5,7 @@ import BigNumber from "bignumber.js";
 import { faServer } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { ETH_NETWORK_LABEL } from "../../../environmentVariables";
 import { DarknodeDetails } from "../../../reducers/types";
 import { darknodeIDHexToBase58 } from "../../pages/Darknode";
 import { Block, BlockBody, BlockTitle } from "./Block";
@@ -38,9 +39,11 @@ export const NetworkBlock = (props: NetworkBlockProps) => {
                 <div className="network-block--info">
                     <table className="darknode-info">
                         <tbody>
+                            <tr><td>Network</td><td>{ETH_NETWORK_LABEL}</td></tr>
                             <tr><td>ID</td><td>{darknodeIDBase58}</td></tr>
                             <tr><td>Address</td><td>{darknodeDetails.ID}</td></tr>
                             <tr><td>Public Key</td><td>{darknodeDetails.publicKey}</td></tr>
+                            <tr><td>Operator</td><td>{darknodeDetails.operator}</td></tr>
                         </tbody>
                     </table>
                 </div>
