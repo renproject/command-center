fa4ae80bff5dfba9eec216b160d987bde26805a6
noiach <gh@noiach.com>
2019-07-26 16:14:12 +1000

    Add title to map

--- src/components/darknodeMap/darknodeMap.tsx
+++ src/components/darknodeMap/darknodeMap.tsx
@@ -22,62 +22,66 @@ export const DarknodeMap = () => {
 
     return (
         <div className="map container">
-            <ComposableMap
-                className="map--world"
-                projectionConfig={{ scale: 205 }}
-                width={980}
-                height={551}
-                style={{
-                    width: "100%",
-                    height: "auto",
-                }}
-            >
-                <ZoomableGroup center={[0, 20]} disablePanning>
-                    <Geographies geography={MapJSON}>
-                        {(geographies, projection) =>
-                            geographies.map((geography, i) =>
-                                // Don't render Antarctica
-                                (geography as { id: string }).id !== "ATA" && (
-                                    <Geography
-                                        key={i}
-                                        geography={geography}
-                                        projection={projection}
-                                        style={{
-                                            default: {
-                                                fill: "#00244d",
-                                                stroke: "#00244d",
-                                                strokeWidth: 0,
-                                                outline: "none",
-                                            },
-                                            hover: {
-                                                fill: "#00244d",
-                                                stroke: "#00244d",
-                                                strokeWidth: 0,
-                                                outline: "none",
-                                            },
-                                            pressed: {
-                                                fill: "#00244d",
-                                                stroke: "#00244d",
-                                                strokeWidth: 0,
-                                                outline: "none",
-                                            },
-                                        }}
+            <hr />
+            <h2>DARKNODES ONLINE</h2>
+            <div className="map--world--outer">
+                <ComposableMap
+                    className="map--world"
+                    projectionConfig={{ scale: 205 }}
+                    width={980}
+                    height={551}
+                    style={{
+                        width: "100%",
+                        height: "auto",
+                    }}
+                >
+                    <ZoomableGroup center={[0, 20]} disablePanning>
+                        <Geographies geography={MapJSON}>
+                            {(geographies, projection) =>
+                                geographies.map((geography, i) =>
+                                    // Don't render Antarctica
+                                    (geography as { id: string }).id !== "ATA" && (
+                                        <Geography
+                                            key={i}
+                                            geography={geography}
+                                            projection={projection}
+                                            style={{
+                                                default: {
+                                                    fill: "#00244d",
+                                                    stroke: "#00244d",
+                                                    strokeWidth: 0,
+                                                    outline: "none",
+                                                },
+                                                hover: {
+                                                    fill: "#00244d",
+                                                    stroke: "#00244d",
+                                                    strokeWidth: 0,
+                                                    outline: "none",
+                                                },
+                                                pressed: {
+                                                    fill: "#00244d",
+                                                    stroke: "#00244d",
+                                                    strokeWidth: 0,
+                                                    outline: "none",
+                                                },
+                                            }}
+                                        />
+                                    ))}
+                        </Geographies>
+                        <Markers>
+                            {counter.darknodes.map((darknode, i) => <Marker key={i} marker={darknode}>
+                                <Link to={`/darknode/${darknode.darknodeID}`}>
+                                    <circle
+                                        cx={0}
+                                        cy={0}
+                                        r={5}
                                     />
-                                ))}
-                    </Geographies>
-                    <Markers>
-                        {counter.darknodes.map((darknode, i) => <Marker key={i} marker={darknode}>
-                            <Link to={`/darknode/${darknode.darknodeID}`}>
-                                <circle
-                                    cx={0}
-                                    cy={0}
-                                    r={5}
-                                />
-                            </Link>
-                        </Marker>)}
-                    </Markers>
-                </ZoomableGroup>
-            </ComposableMap>
-        </div >
+                                </Link>
+                            </Marker>)}
+                        </Markers>
+                    </ZoomableGroup>
+                </ComposableMap>
+            </div>
+        </div>
     );
 };
