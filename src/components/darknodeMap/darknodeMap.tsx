7d5d20a8e818b700296ccdfe55f3e7a4ef7bebfe
noiach <gh@noiach.com>
2019-07-26 15:54:11 +1000

    Fetch darknode locations using lightnode and geolocation api

--- src/components/darknodeMap/darknodeMap.tsx
+++ src/components/darknodeMap/darknodeMap.tsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
     ComposableMap, Geographies, Geography, Marker, Markers, ZoomableGroup,
 } from "react-simple-maps";
-import { Link } from "react-router-dom";
 
 import { MapContainer } from "./mapContainer";
 import MapJSON from "./world-50m.json";
 
 export const DarknodeMap = () => {
     const counter = MapContainer.useContainer();
+
+    // useEffect replaces `componentDidMount` and `componentDidUpdate`.
+    // To limit it to running once, we use the initialized hook.
+    const [initialized, setInitialized] = React.useState(false);
+    React.useEffect(() => {
+        if (!initialized) {
+            setInitialized(true);
+            counter.fetchDarknodes().catch(console.error);
+        }
+    }, [initialized]);
+
     return (
         <div className="map container">
             <ComposableMap
@@ -24,7 +35,7 @@ export const DarknodeMap = () => {
                 <ZoomableGroup center={[0, 20]} disablePanning>
                     <Geographies geography={MapJSON}>
                         {(geographies, projection) =>
-                            geographies.map((geography: any, i) =>
+                            geographies.map((geography, i) =>
                                 // Don't render Antarctica
                                 (geography as { id: string }).id !== "ATA" && (
                                     <Geography
