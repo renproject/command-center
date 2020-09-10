ccaded8bce1ba3776bfef7fe49aac43bfd1787ff
tomasz-rozmus <tomasz.rozmus@gmail.com>
2020-09-10 14:39:19 +0200

    feat(fe) news section

--- src/controllers/common/header/Header.tsx
+++ src/controllers/common/header/Header.tsx
@@ -20,10 +20,10 @@ import { ReactComponent as RenVMIcon } from "../../../styles/images/Icon-HyperDr
 import { ReactComponent as IntegratorsIcon } from "../../../styles/images/icon-integrators.svg";
 import { ReactComponent as NetworkIcon } from "../../../styles/images/icon-network.svg";
 import { ReactComponent as OverviewIcon } from "../../../styles/images/Icon-Overview.svg";
-import { ExternalLink, URLs } from "../../../views/ExternalLink";
 import { StatusDot, StatusDotColor } from "../../../views/StatusDot";
 import { AccountDropdown } from "./AccountDropdown";
 import { MoreDropdown } from "./MoreDropdown";
+import { NewsBanner } from "./NewsBanner";
 
 const getCurrencyOptions = () => {
   const options = new Map<string, React.ReactNode>();
@@ -101,10 +101,9 @@ export const Header = () => {
         <div className="header--logo" />
       </Link>
       {renNetwork.name === "mainnet" ? (
-        <ExternalLink href={URLs.welcomeToCommandCenter} className="new">
-          <span className="new-new xl-or-larger">New</span>
-          <span className="new-blue">Mainnet is live. →</span>
-        </ExternalLink>
+        <div className="header--news">
+            <NewsBanner />
+        </div>
       ) : (
         <div className="new">
           <div className="header--network">
--- src/controllers/common/header/NewsBanner.tsx
+++ src/controllers/common/header/NewsBanner.tsx
@@ -1,17 +1,21 @@
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
 import { fetchMediumPosts, MediumPost } from "../../../lib/medium/medium";
+import { classNames } from "../../../lib/react/className";
 import { ExternalLink } from "../../../views/ExternalLink";
 
 export const NewsBanner: React.FC = () => {
-  const freshness = 3;
-  const [loading, setLoading] = useState(true);
+  const freshness = 3; // days
+  const take = 4;
   const [mediumPosts, setMediumPosts] = useState<MediumPost[]>([]);
 
   useEffect(() => {
     fetchMediumPosts("renproject").then((posts) => {
-      setMediumPosts(posts);
+      setMediumPosts(posts.slice(0, take));
     });
   }, []);
 
@@ -32,24 +36,36 @@ export const NewsBanner: React.FC = () => {
 
   const generateLink = (post: MediumPost): JSX.Element => {
     const unixDate = moment(post["atom:updated"][0]).unix() * 1000;
+    const isNewPost = isNew(unixDate);
     return (
-      <div className="medium-banner--link content--links" key={unixDate}>
-        <ExternalLink href={post.link[0]}>
-          <span className={isNew(unixDate) ? "new" : ""}>
-            {post.title[0]} &rarr;
-          </span>
-        </ExternalLink>
+      <div key={unixDate} className="news--slider--slide">
+        <div className="news--slider--slide--content">
+          <ExternalLink href={post.link[0]} className="new">
+            <span className={classNames("new-blue", isNewPost ? "is-new" : "")}>
+              {post.title[0]} &rarr;
+            </span>
+          </ExternalLink>
+        </div>
       </div>
     );
   };
 
-  if (loading) {
-    return null;
-  }
-
   return (
-    <Slider autoplay autoplaySpeed={5000} pauseOnFocus>
-      {mediumPosts.map(generateLink)}
-    </Slider>
+    <div
+      className={classNames(
+        "news--slider--container",
+        mediumPosts.length ? "loaded" : ""
+      )}
+    >
+      <Slider
+        autoplay
+        autoplaySpeed={5000}
+        pauseOnFocus
+        arrows={false}
+        dots={false}
+      >
+        {mediumPosts.map(generateLink)}
+      </Slider>
+    </div>
   );
 };
